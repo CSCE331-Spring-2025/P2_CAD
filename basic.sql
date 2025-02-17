34f529daff87f94e52942022ee4cf65d3f5b12e9
Chloe Lee <ljwon2002@tamu.edu>
2025-02-17 13:50:35 -0600

    initial

---
+++ basic.sql
@@ -0,0 +1,53 @@
+CREATE TABLE Employee (
+    Employee_ID INT PRIMARY KEY,
+    First_name CHAR(50) NOT NULL,
+    Last_name CHAR(50) NOT NULL,
+    Position CHAR(50) NOT NULL,
+    Order_ID INT,
+    FOREIGN KEY (Order_ID) REFERENCES customer_order(Order_ID)
+);
+
+CREATE TABLE Customer (
+    Customer_ID INT PRIMARY KEY,
+    Phone_number INT NOT NULL,
+    Order_ID INT,
+    FOREIGN KEY (Order_ID) REFERENCES customer_order(Order_ID)
+);
+
+CREATE TABLE customer_order (
+    Order_ID INT PRIMARY KEY,
+    Time TIMESTAMP NOT NULL,
+    Total_Price FLOAT NOT NULL
+);
+
+CREATE TABLE Menu_Item (
+    Menu_ID INT PRIMARY KEY,
+    Price FLOAT NOT NULL,
+    Menu_Inventory INT,
+    FOREIGN KEY (Menu_Inventory) REFERENCES Inventory(Inventory_ID)
+);
+
+CREATE TABLE Inventory (
+    Inventory_ID INT PRIMARY KEY,
+    Current_Number INT NOT NULL,
+    Name CHAR(100) NOT NULL,
+    Supply_Order_History INT
+);
+
+-- Customer / Menu Junction Table
+CREATE TABLE C_M_Junction (
+    ID INT PRIMARY KEY,
+    Menu_ID INT,
+    Order_ID INT,
+    FOREIGN KEY (Menu_ID) REFERENCES Menu_Item(Menu_ID),
+    FOREIGN KEY (Order_ID) REFERENCES customer_order(Order_ID)
+);
+
+-- Menu / Inventory Junction Table
+CREATE TABLE M_I_Junction (
+    MI_ID INT PRIMARY KEY,
+    Menu_ID INT,
+    Inventory_ID INT,
+    FOREIGN KEY (Menu_ID) REFERENCES Menu_Item(Menu_ID),
+    FOREIGN KEY (Inventory_ID) REFERENCES Inventory(Inventory_ID)
+);
