682032de30d97124491f9454318572da5e629073
sareemmomin21 <Sareem.momin04@gmail.com>
2025-02-24 10:32:35 -0600

    Removed dbSetup.java from the repository

--- dbSetup.java
+++
@@ -1,9 +0,0 @@
-/*
-CSCE 315
-Database setup example
-Purpose: Keeping usernames and password private.
-*/
-public final class dbSetup  {
-  public static final String user = "team_cad";
-  public static final String pswd = "cad";
-}//end class
