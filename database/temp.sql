03b3b233be7fb331a2de5e3cda1edb93284a1d68
Chloe Lee <ljwon2002@tamu.edu>
2025-02-24 11:12:52 -0600

    directory

---
+++ database/temp.sql
@@ -0,0 +1,45 @@
+-- first page
+
+-- login with pin (employee id)
+    -- CREATE VIEW
+    -- manager view
+        -- employee
+        -- inventory
+        -- order history
+            -- refund
+            -- trend >> based on this, create a suggestion for inventory
+        -- order
+    -- cashier view
+        -- order
+        -- order history // refund?
+
+------------------------------------------------------------------
+-- order page
+
+-- left
+    -- get info from menu-item
+
+-- right
+    -- create new order table
+    -- track that value
+
+-- payment page
+    -- get order info from previous page
+    -- cash/ card
+    -- status
+
+
+------------------------------------------------------------------
+
+-- inventory page
+
+
+-- show basic chart : name, quantity, which menu is related(using junction table), category (general / ingredient)
+
+-- how many of those are needed in a week 
+
+----------------------------------------------------------------
+
+-- employee page
+
+-- 
