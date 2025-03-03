e50537a7cb6192c7dee9af0e6fbf98a51ee1edea
Rayan Ali <rayanamirali@gmail.com>
2025-03-02 21:39:33 -0600

    added an interaction java file for when making the POS look better and use this to organize how we make the interactive POS system rather than going to different java files and just import all the classes into one java file for desinging after functionalities work correctly as they should.

---
+++ InteractionPage.java
@@ -0,0 +1,4 @@
+public class InteractionPage {
+    // Add all functionalities here (all the classes and design each tab when clicked on here)
+    // Will help with organizing the code and making it all in one spot rather than all over the place
+}
