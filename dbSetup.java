f254edaee1fbdf76f17af65fe94b95ef2776dc38
sareemmomin21 <Sareem.momin04@gmail.com>
2025-03-09 12:02:33 -0500

    Hid dbSetup again

--- dbSetup.java
+++ dbSetup.java
@@ -1,9 +1,4 @@
-// Source code is decompiled from a .class file using FernFlower decompiler.
-public final class dbSetup {
-    public static final String user = "team_cad";
-    public static final String pswd = "cad";
- 
-    public dbSetup() {
-    }
- }
- 
+public class dbSetup {
+    public static final String user = "your_user";
+    public static final String pswd = "your_password";
+}
